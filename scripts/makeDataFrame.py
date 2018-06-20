9604d9696071630617dab0bfb6a730305ebbab2e
Eric Pooser <pooser@jlab.org>
2018-06-20 14:55:52 -0400

    update scripts

--- scripts/makeDataFrame.py
+++ scripts/makeDataFrame.py
@@ -5,16 +5,14 @@ import string, glob, pickle
 import pandas as pd
 
 # Report file dictionary
-rfd = { 'rn'           : [],  # run number
-        'pcent'        : [],  # central momentum
-        'i4a'          : [],  # bcm4a current (uA, cut > 5 uA)
-        'i4b'          : [],  # bcm4b current (uA, cut > 5 uA)
-        'clt'          : [],  # computer live time
-        'pt2'          : [],  # EL-REAL (pTRIG2) rate
-        'tr_eff'       : [],  # tracking efficiency
-        'etr_eff'      : [],  # electron tracking efficiency
-        'htr_eff'      : [],  # hadron tracking efficiency
-        'scin_eff'     : [] } # 3/4 trigger efficiency
+rfd = { 'rn'       : [],  # run number
+        'i4a'      : [],  # bcm4a current (uA, cut > 5 uA)
+        'i4b'      : [],  # bcm4b current (uA, cut > 5 uA)
+        'clt'      : [],  # computer live time
+        'psf'      : [],  # EL-REAL (pTRIG2) pre-scale factor
+        'pt2r'     : [],  # EL-REAL (pTRIG2) rate
+        'etr_eff'  : [],  # electron tracking efficiency
+        'scin_eff' : [] } # 3/4 trigger efficiency
         
 # Populate list of report files from 21 degrees
 rf = glob.glob('../all-reports/replay_shms_production_*_-1.report')
@@ -27,22 +25,21 @@ for index, run in enumerate(rf):
             data = line.split(':')
             # Kinematic configurations
             if ('Run Num'  in data[0]) : rfd['rn'].append(data[1].strip())
-            if ('Momentum' in data[0]) : rfd['pcent'].append(data[1].strip())
             # Charge and current
             if ('BCM4a Beam Cut Current' in data[0]) : rfd['i4a'].append(filter(lambda x: x in string.digits + '.', data[1]))
             if ('BCM4b Beam Cut Current' in data[0]) : rfd['i4b'].append(filter(lambda x: x in string.digits + '.', data[1]))
             # Live times (must be multiplied by 0.01 -> done later)
-            if ('Pre-Scaled Ps2 SHMS Computer Live Time' in data[0])   : rfd['clt'].append(data[1][:8].strip())
+            if ('Pre-Scaled Ps2 SHMS Computer Live Time' in data[0]) : rfd['clt'].append(data[1][:8].strip())
             # Tracking efficiencies
-            if ('SING FID TRACK EFFIC' in data[0]) and (data[0].count(' ') == 14) : rfd['tr_eff'].append(data[1][:8].strip())
-            if ('E SING FID TRACK EFFIC' in data[0])      : rfd['etr_eff'].append(data[1][:8].strip())
-            if ('HADRON SING FID TRACK EFFIC' in data[0]) : rfd['htr_eff'].append(data[1][:8].strip())
+            if ('E SING FID TRACK EFFIC' in data[0]) : rfd['etr_eff'].append(data[1][:8].strip())
             # Trigger efficiency
             if ('3_of_4 EFF' in data[0]) : rfd['scin_eff'].append(data[1].strip())
             # EL-REAL trigger rate
             if ('pTRIG2' in data[0]) : 
                pt2b = data[1][data[1].find('[')+1:data[1].find(']')]
-               rfd['pt2'].append(filter(lambda x: x in string.digits + '.', pt2b))
+               rfd['pt2r'].append(filter(lambda x: x in string.digits + '.', pt2b))
+            psdata = data[0].split('=')
+            if ('Ps2_factor' in psdata[0]) : rfd['psf'].append(psdata[1].strip())
             
 # Create pandas data frame from report file dictionary
 # By default the keys of the dict become the DataFrame columns
--- scripts/makePlots.py
+++ scripts/makePlots.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # Import various modules
-import pickle
+import pickle, copy
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -18,24 +18,24 @@ mpl.rcParams.update({'errorbar.capsize': 10})
 dd = pickle.load(open('../ddicts/dataDictXbj.pkl', 'rb'))
 
 # Make simple overlay plot of E' yield for the c12 target, recall there are 2 momentum settings
-# plt.figure()
-# plt.title('Charge Normalized Yields for 12C')
-# plt.xlim(3.5, 6.1) 
-# plt.ylim(0.0, np.max(dd['c12']['eprime_qny'][0]) + np.max(dd['c12']['eprime_qny'][0])*0.10)
-# plt.xlabel('E\' / 0.100 GeV')
-# plt.ylabel('Yield')
-# plt.bar(dd['c12']['eprime_val'][0], dd['c12']['eprime_qny'][0],
-#         yerr = dd['c12']['eprime_qny_err'][0], label = '4.0 GeV',
-#         width = 0.100, align = 'center', color = 'b', edgecolor = 'b', alpha = 0.5)
-# plt.bar(dd['c12']['eprime_val'][1], dd['c12']['eprime_qny'][1],
-#         yerr = dd['c12']['eprime_qny_err'][1], label = '5.1 GeV',
-#         width = 0.100, align = 'center', color = 'g', edgecolor = 'g', alpha = 0.5)
-# plt.legend(loc = 'best', numpoints = 1, fancybox = True)
-# plt.savefig('../plots/c12-qny-simple.png')
+plt.figure()
+plt.title('Charge Normalized Yields for 12C')
+plt.xlim(3.5, 6.1) 
+plt.ylim(0.0, np.max(dd['c12']['eprime_qny'][0]) + np.max(dd['c12']['eprime_qny'][0])*0.10)
+plt.xlabel('E\' / 0.100 GeV')
+plt.ylabel('Yield')
+plt.bar(dd['c12']['eprime_val'][0], dd['c12']['eprime_qny'][0],
+        yerr = dd['c12']['eprime_qny_err'][0], label = '4.0 GeV',
+        width = 0.100, align = 'center', color = 'b', edgecolor = 'b', alpha = 0.5)
+plt.bar(dd['c12']['eprime_val'][1], dd['c12']['eprime_qny'][1],
+        yerr = dd['c12']['eprime_qny_err'][1], label = '5.1 GeV',
+        width = 0.100, align = 'center', color = 'g', edgecolor = 'g', alpha = 0.5)
+plt.legend(loc = 'best', numpoints = 1, fancybox = True)
+plt.savefig('../plots/c12-qny-simple.png')
 
 # =:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=
 
-# Make overlay plot of xbj yields with data points
+# # Make overlay plot of xbj yields with data points
 # tarfont = {'family' : 'serif',
 #            'color'  : 'darkred',
 #            'weight' : 'normal',
@@ -77,58 +77,58 @@ dd = pickle.load(open('../ddicts/dataDictXbj.pkl', 'rb'))
 
 # =:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=
 
-# Open data frame produced via makeDataFrame.py
-df = pickle.load(open('../dframes/dataFrame.pkl', 'rb'))
-# Plot variables in data frame
-mkrs = ['bd', 'gs', 'ko', 'r*']
-fig, axes = plt.subplots(ncols = 2, figsize  = (25, 15)) 
-# Unpack the axs array into its individual components
-ax0, ax1 = axes.flatten()
+# # Open data frame produced via makeDataFrame.py
+# df = pickle.load(open('../dframes/dataFrame.pkl', 'rb'))
+# # Plot variables in data frame
+# mkrs = ['bd', 'gs', 'ko', 'r*']
+# fig, ax = plt.subplots(ncols = 2, figsize  = (20, 10)) 
+# # Unpack the axs array into its individual components
+# ax0, ax1 = ax.flatten()
+# # Plot trigger rate vs computer live time
+# df.plot(x = 'rn', y = 'clt', ax = ax0, style = mkrs[0], label = 'Comp Live Time', 
+#         legend = False, ms = 10, alpha = 0.75)
+# ax0.set_title('Trigger Rate \& Computer Live Time')
+# ax0.set_xlabel('Run Number')
+# ax0.set_ylabel('Live Time (\%)', color = 'b')
+# ax0.tick_params('y', colors = 'b')
+# ax0.set_ylim(55.0, 105.0)
+# ax0.set_xlim(2480, 2567)
+# ax0.xaxis.grid(True)
+# ax0p1 = ax0.twinx()
+# df.plot(x = 'rn', y = 'pt2r', ax = ax0p1, style = mkrs[1], label = 'Trigger Rate', 
+#         legend = False, ms = 10, alpha = 0.75)
+# ax0p1.set_ylim(0.0, 40.0)
+# ax0p1.set_xlim(2480, 2567)
+# ax0p1.set_ylabel('Trigger Trigger Rate (kHz)', color = 'g')
+# ax0p1.tick_params('y', colors = 'g')
 
-# Plot trigger rate vs computer live time
-df.plot(x = 'rn', y = 'clt', ax = ax0, style = mkrs[0], label = 'Comp Live Time', 
-        legend = False, ms = 10, alpha = 0.75)
-ax0.set_title('Trigger Rate \& Computer Live Time')
-ax0.set_xlabel('Run Number')
-ax0.set_ylabel('Live Time (\%)', color = 'b')
-ax0.tick_params('y', colors = 'b')
-ax0.set_ylim(55.0, 105.0)
-ax0.set_xlim(2480, 2567)
-ax0p1 = ax0.twinx()
-df.plot(x = 'rn', y = 'pt2', ax = ax0p1, style = mkrs[1], label = 'EL-REAL Rate', 
-        legend = False, ms = 10, alpha = 0.75)
-ax0p1.set_ylim(0.0, 40.0)
-ax0p1.set_ylabel('EL-REAL Trigger Rate (kHz)', color = 'g')
-ax0p1.tick_params('y', colors = 'g')
-ax0p1.set_xlim(2480, 2567)
-
-# Plot total charge as measured by BCMs
-df.plot(x = 'rn', y = 'i4a', ax = ax1, style = mkrs[2], alpha = 0.75, 
-        label = 'BCM 4A', ms = 10)
-df.plot(x = 'rn', y = 'i4b', ax = ax1, style = mkrs[3], alpha = 0.75, 
-        label = 'BCM 4B', ms = 10)
-ax1.set_ylim(20, 60)
-ax1.set_xlim(2480, 2567)
-ax1.set_title('BCM Current')
-ax1.set_xlabel('Run Number')
-ax1.set_ylabel('Current (uA)')
-ax1.grid()
-ax1.set_xticklabels([])
-ax1.legend(numpoints = 1, loc = 'best')
-# Calculate the residual between the two BCM's and plot
-df['i4a_i4b_resid'] = df['i4a'] - df['i4b']
-divider  = make_axes_locatable(ax1)
-ax1resid = divider.append_axes("bottom", size = '50%', pad = 0)
-ax1.figure.add_axes(ax1resid)
-df.plot(x = 'rn', y = 'i4a_i4b_resid', ax = ax1resid, style = 'm-', 
-        label = 'BCM4A - BCM4B')
-ax1resid.set_ylim(0.0, 1)
-ax1resid.set_xlim(2480, 2567)
-ax1resid.set_xlabel('Run Number')
-ax1resid.set_ylabel('Residual (uA)')
-ax1resid.grid()
-ax1resid.locator_params(axis = 'y', nbins = 6)
-plt.tight_layout()
-plt.savefig('../plots/efficiencies.png')
+# # Plot total charge as measured by BCMs
+# df.plot(x = 'rn', y = 'i4a', ax = ax1, style = mkrs[2], alpha = 0.75, 
+#         label = 'BCM 4A', ms = 10)
+# df.plot(x = 'rn', y = 'i4b', ax = ax1, style = mkrs[3], alpha = 0.75, 
+#         label = 'BCM 4B', ms = 10)
+# ax1.set_ylim(20, 60)
+# ax1.set_xlim(2480, 2567)
+# ax1.set_title('BCM Current')
+# ax1.set_xlabel('Run Number')
+# ax1.set_ylabel('Current (uA)')
+# ax1.grid()
+# ax1.set_xticklabels([])
+# ax1.legend(numpoints = 1, loc = 'best')
+# # Calculate the residual between the two BCM's and plot
+# df['i4a_i4b_resid'] = df['i4a'] - df['i4b']
+# divider  = make_axes_locatable(ax1)
+# ax1p1 = divider.append_axes("bottom", size = '50%', pad = 0)
+# ax1.figure.add_axes(ax1p1)
+# df.plot(x = 'rn', y = 'i4a_i4b_resid', ax = ax1p1, style = 'm-', 
+#         label = 'BCM4A - BCM4B')
+# ax1p1.set_ylim(0.0, 1)
+# ax1p1.set_xlim(2480, 2567)
+# ax1p1.set_xlabel('Run Number')
+# ax1p1.set_ylabel('Residual (uA)')
+# ax1p1.grid()
+# ax1p1.locator_params(axis = 'y', nbins = 6)
+# plt.tight_layout()
+# plt.savefig('../plots/run-vars.png')
 
 plt.show()
